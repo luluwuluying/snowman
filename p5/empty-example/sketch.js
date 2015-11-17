112daffa2a4eb8d8ec656e60cf5b03d6d2b73dd3
luluwuluying <443726224@qq.com>
2015-11-17 17:56:37 -0500

    first draft
    movement

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,9 +1,57 @@
+var bg;
+var snowman;
+var sun;
+var snowflake;
+
+
+
 function setup() {
-  // uncomment this line to make the canvas the full size of the window
-  // createCanvas(windowWidth, windowHeight);
+  
+   createCanvas(windowWidth, windowHeight);
+   bg = loadImage("../images/background.svg");
+   snowman = createSprite(windowWidth/2, windowHeight-100,50,50);
+   snowman.addAnimation("moving","../images/s1.svg");
+   snowman.addAnimation("stand","../images/s3.svg");
+   
+    
+    
+
 }
 
 function draw() {
-  // draw stuff here
-  // ellipse(width/2, height/2, 50, 50);
+    background(bg);
+    
+    
+    
+
+    drawSprites();
+}
+
+function keyPressed(){
+    
+    if(keyCode === LEFT_ARROW){
+        snowman.changeAnimation("moving");
+       snowman.mirrorX(-1);
+        snowman.velocity.x = - 3;
+    }
+    
+    
+    else if(keyCode === RIGHT_ARROW){
+        snowman.changeAnimation("moving");
+        snowman.mirrorX(1);
+        snowman.velocity.x = 3;
+    }
+    
+    else {
+    snowman.changeAnimation('stand');
+    snowman.velocity.x = 0;
+  }
+}
+
+function keyReleased() {
+    
+    snowman.changeAnimation('stand');
+    snowman.velocity.x = 0;
+    
+    
 }
