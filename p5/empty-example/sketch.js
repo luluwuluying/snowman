ef2d2b570249e0e550d597a83a7a59e081624568
luluwuluying <443726224@qq.com>
2015-12-13 23:00:35 -0500

    change3

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -4,6 +4,9 @@ var sun;
 var snowflake;
 var hearts;
 var speed = 1;
+var score;
+var snowX;
+var sunX;
 
 
 function setup() {
@@ -15,6 +18,7 @@ function setup() {
    snowman.addAnimation("floating","../images/s2.svg");
    snowman.addAnimation("moving","../images/s1.svg");
    
+   
     sun = new Group();
     snowflake = new Group();
     hearts = new Group();
@@ -24,6 +28,10 @@ function setup() {
         newHearts.addAnimation("heart","../images/heart.svg");
         hearts.add(newHearts);
     }
+    
+     score=0;
+    
+    
           
 }
 
@@ -33,64 +41,85 @@ function draw() {
     background(bg);
     textSize(20);
     text("Life:" ,20,30);
-
+    textSize(50);
+    text(score,20,80);
+    
     for(var i=0;i<sun.length;i++){
         var s = sun[i];
-       // s.addSpeed(0.01,90);
-        
+       
         
         if (s.position.y >height) {
             s.position.y = 0;
         }     
         
     }
+    
 
     for(var i=0;i<snowflake.length;i++){
         var f = snowflake[i];
-        //f.addSpeed(0.01,90);
-        
+      
         if (f.position.y >height)
             
             f.position.y=0;
     }
     
-    snowman.collide(sun);
-    snowman.overlap(sun,collect);
+    sun.collide(snowman, collect);
+    snowflake.collide(snowman, collect);
+    sun.overlap(snowman);
+    snowflake.overlap(snowman);
     
-    snowman.collide(snowflake);
-    snowman.overlap(snowflake,collect);
 
+        if(snowman.position.x <100)snowman.position.x=100;
+        if(snowman.position.x >width -100)snowman.position.x=width -100;
+    
+    
     drawSprites();
     
-    if(frameCount%360==0){
-        speed = speed * 1.1;
-    var newSnowflake = createSprite(random(0,width), random(0,100));
+    snowX=random(width);
+    sunX=random(width);
+    
+    if(frameCount%180==0){
+        speed = speed * 1;
+    var newSnowflake = createSprite(snowX, random(0,100));
         newSnowflake.addAnimation("snow","../images/snowflake.svg");
         newSnowflake.rotationSpeed = -2;
         newSnowflake.setVelocity(0, speed);
-//        newSnowflake.addSpeed(0.01,90);
         snowflake.add(newSnowflake);
     
-     var newSun = createSprite(random(0,width), random(0,100));
+    
+    }
+    
+    if(frameCount%540==0){
+        speed = speed * 1;
+    
+    
+     var newSun = createSprite(sunX, random(0,100));
         newSun.addAnimation("sun","../images/sun.svg");
-//        newSun.addSpeed(0.01, 90);
-        newSun.setVelocity(0,2*speed);
+        newSun.setVelocity(0,speed);
+//        if(newSun.position.x-newSnowflake.position.x)
         sun.add(newSun);
     }
     
+    
+    if(Math.abs(snowX-sunX)<100){
+        snowX=random(width);
+        sunX=random(width);
+    }
+    
+      
 }
 
 function keyPressed(){
     
     if(keyCode === LEFT_ARROW){
         snowman.changeAnimation("moving");
-        snowman.velocity.x = - 10;
+            snowman.velocity.x = - 10;
     }
     
     
     else if(keyCode === RIGHT_ARROW){
         snowman.changeAnimation("moving");
-        snowman.velocity.x = 10;
+            snowman.velocity.x = 10;
     }
     
     else {
@@ -109,21 +138,58 @@ function keyReleased() {
 
 function collect(collector, collected){
     
-//    collector.changeAnimation("stretch");
-//    collector.animation.rewind();
-    console.log(123);
-    if (collected.getAnimationLabel()=="sun"){
+
+    if (collector.getAnimationLabel()=="sun"){
         snowman.changeAnimation("floating");
         hearts[hearts.length-1].remove();
     }
-    collector.position.y = windowHeight-100;
-    collected.remove();
+//    collector.position.y = windowHeight-100;
+    collector.remove();
+    
+    if(collector.getAnimationLabel()=="snow"){
+        score++;
+    }
     
     if (hearts.length < 1 ){
         alert("Game over!");
+        reset();
+        collected.remove();
+        collector.remove();
     }
     
 }
+
+function reset(){
+    
+      for(var i=0;i<5;i++){
+        var newHearts = createSprite(90+i*40,20);
+        newHearts.addAnimation("heart","../images/heart.svg");
+        hearts.add(newHearts);
+    
+    } 
+    for(var i=0;i<sun.length;i++){
+        sun[0].remove();
+    }
+    for(var i=0;i<snowflake.length;i++){
+        snowflake[0].remove();
+    }
+        
+        snowman.changeAnimation('stand');
+        snowman.velocity.x = 0;
+        snowman = createSprite(windowWidth/2, windowHeight-100,50,50);
+        snowman.addAnimation("stand","../images/s3.svg");
+       snowman.addAnimation("floating","../images/s2.svg");
+       snowman.addAnimation("moving","../images/s1.svg");
+        
+        
+        speed = 1;
+    
+        score = 0;
+        
+    
+    
+}
+
 
 
 
